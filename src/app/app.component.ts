2783c79576ea23aa63f2bf25aeca5094681f65d6
Nataliya Uzva <ashatan_u@mail.ru>
2019-03-19 20:48:46 +0200

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-wert',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  index = 0;
   title = 'test-angular-source-it';
+
+  users = ['Joun', 'Vasya', 'Rosa'];
+
+  remove(currentUser) {
+    this.users = this.users
+      .filter(user => user !== currentUser);
+  }
+
+  edit(user, index, input) {
+    this.index = index;
+    input.value = user;
+  }
+
+  addUser(input) {
+    this.users.push(input.value);
+    input.value = '';
+  }
+
+  editUser(input) {
+    this.users[this.index - 1] = input.value;
+    this.index = 0;
+    input.value = '';
+  }
 }
